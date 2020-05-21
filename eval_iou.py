9a7f4d956bc082ad65bbe709d2c7c67ddae04cef
KnowingNothing <zhengsizeMax@outlook.com>
2020-05-21 15:27:20 +0800

    fix iou

--- eval_iou.py
+++ eval_iou.py
@@ -1,16 +1,24 @@
+import os
 import argparse
 import torch
 import time
+import numpy as np
 from dilated_resnet import CAM
 from torch.utils.data import DataLoader
 from dataset import DetPixelAnnotationSet
+from PIL import Image
+
+
+im_dir = "peek_image"
+if not (os.path.exists(im_dir) and os.path.isdir(im_dir)):
+  os.mkdir(im_dir)
 
 
 batch_size = 1
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument("-m", "--model", help="model path", type=str, required=True)
+  parser.add_argument("-m", "--model", help="model path", type=str, default="")
   parser.add_argument("-c", "--classes", help="how many classes", type=int, default=200)
   parser.add_argument("-g", "--gpu", help="use gpu", action="store_true")
   parser.add_argument("--skip_size", help="skip data with height/width larger than this value", type=int, default=1000)
@@ -18,7 +26,8 @@ if __name__ == "__main__":
 
   print("Preparing model...")
   model = CAM(num_classes=args.classes)
-  model.load_state_dict(torch.load(args.model, map_location=lambda storage, loc: storage))
+  if args.model != "":
+    model.load_state_dict(torch.load(args.model, map_location=lambda storage, loc: storage))
   if args.gpu:
     model = model.cuda()
 
@@ -31,15 +40,15 @@ if __name__ == "__main__":
   count_batch = 0
 
   model.eval()
-  total_iou = torch.zeros([batch_size, args.classes+1]).float()
+  total_iou = [[] for i in range(args.classes)]
   label_mark = torch.arange(0, args.classes+1).view(1, -1, 1, 1).int()
   if args.gpu:
-    total_iou = total_iou.cuda()
     label_mark = label_mark.cuda()
   print("Begin caluculating...")
   begin = time.time()
   with torch.no_grad():
     for batch_data in data_loader:
+      count_batch += 1
       if args.gpu:
         if batch_data["data"].shape[2] > args.skip_size and batch_data["data"].shape[3] > args.skip_size:
           print("skip a data with shape:", batch_data["data"].shape)
@@ -55,43 +64,65 @@ if __name__ == "__main__":
       if args.gpu:
         mask = mask.cuda()
       # print(mask.shape)
-      cam_map = torch.nn.functional.upsample_bilinear(cam_map, mask.shape)
-      cam_map = cam_map > 0
+      # print(mask.sum())
+
+      cam_map_org = torch.nn.functional.upsample_bilinear(cam_map, mask.shape)
+      cam_map = cam_map_org > 0
       background = cam_map.int().sum(dim=1, keepdim=True) == 0
       cam_map = torch.cat([background, cam_map], dim=1)
       
       tmp = label_mark * torch.ones_like(cam_map).int()
       tmp = mask.int() == tmp.int()
+
+      # predict_map = torch.argmax(torch.cat([background.float(), cam_map_org], dim=1), dim=1).squeeze()
+      # im = Image.fromarray((predict_map.cpu().numpy().astype("float32") / 200 * 255).astype("int32"))
+      # im.save(os.path.join(im_dir, "%d-predict.PNG" % count_batch), "PNG")
+      # im = Image.fromarray((mask.cpu().numpy().astype("float32") / 200 * 255).astype("int32"))
+      # im.save(os.path.join(im_dir, "%d-truth.PNG" % count_batch), "PNG")
+
+      # for i in range(mask.size(0)):
+      #   for j in range(mask.size(1)):
+      #     if mask[i, j] > 0 or predict_map[i, j] > 0:
+      #       print(predict_map[i, j].item(), mask[i, j].item())
+
+      cam_map = cam_map[:, 1:, :, :]
+      tmp = tmp[:, 1:, :, :]
       # print(cam_map.shape, tmp.shape)
       # print(torch.sum(cam_map[:, :1, :, :]), torch.sum(tmp), cam_map.shape)
 
       insert = cam_map.int() * tmp.int()
       # print("insert", insert.int().sum())
-      # for i in range(201):
-      #   for j in range(insert.size(2)):
-      #     for k in range(insert.size(3)):
-      #       print(insert[0, i, j, k], tmp[0, i, j, k])
       # print(torch.any(insert > 0))
       union = (cam_map.int() + tmp.int()) > 0
       # print("union", union.int().sum())
       # print("check insertion:", insert.int().sum(-1).sum(-1).float().cpu())
       # print("check union:", union.int().sum(-1).sum(-1).float().cpu())
-      total_iou = total_iou + (insert.int().sum(-1).sum(-1).float() + 1e-5) / (union.int().sum(-1).sum(-1).float() + 1e-5)
-      count_batch += 1
+      sum_insert = insert.int().sum(-1).sum(-1).float().squeeze()
+      sum_union = union.int().sum(-1).sum(-1).float().squeeze()
+      for c in range(args.classes):
+        if sum_union[c] > 1e-5:
+          total_iou[c].append(sum_insert[c].item() / sum_union[c].item())
+        
       if count_batch % 100 == 0:
         # mean on data
-        iou = total_iou / count_batch
+        class_iou = []
+        for c in range(args.classes):
+          if len(total_iou[c]) > 0:
+            class_iou.append(np.array(total_iou[c]).mean())
         # print("IOU on class:", iou.cpu())
         # mean on class
-        iou = (iou.sum()/(args.classes+1)).item()
+        iou = np.array(class_iou).mean()
         print("parital mean iou over %d batches is: " % count_batch, iou)
       end = time.time()
       # print("calculation of iou done, passing", end - begin, "s")
       begin = end
 
   if count_batch > 0:
-    iou = total_iou / count_batch
-    iou = iou.mean().item()
+    class_iou = []
+    for c in range(args.classes):
+      if len(total_iou[c]) > 0:
+        class_iou.append(np.array(total_iou[c]).mean())
+    iou = np.array(class_iou).mean()
     print("Mean iou is: ", iou)
     with open("iou_record.txt", "a") as fout:
       fout.write("timestamp:%f, mean iou is %f\n" % (time.time(), iou))
